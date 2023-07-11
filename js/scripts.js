243d934de8b6277a090e4118f43b234c43f3ef26
Gustavo Miguel <gustavo.biscaro@gmail.com>
2023-07-11 12:11:05 -0300

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+// Comentário de uma linha
+
+/* 
+Comentário de
+mais de uma 
+linha
+*/
+
+// Variáveis
+/*
+var a = 2;
+var b = 3;
+var soma = a+b;
+var sub = a-b;
+var mult = a*b;
+var divisao = a/b;
+
+alert(soma);
+alert(sub);
+alert(mult);
+alert(divisao);
+*/
+
+// If, Else, Else If 
+/*
+var a = 2;
+var b = 3;
+var c = "Olá Mundo";
+
+if (c%2 == 1) {
+    alert("Número Ímpar");
+} else if (c%2 == 0) {
+    alert("Número Par");
+}
+
+else {
+    alert("Valor Inválido");
+}
+*/
+
+// Laços
+// While (enquanto)
+/*
+var i = 0;
+
+while (i < 3) {
+    alert(i);
+    i++;
+}
+
+
+// For (para)
+var j;
+
+for(j=0;j<3; j++) {
+    alert(j);
+}
+
+*/
+
+// Tipos de dados
+
+/* var numero = 6;
+var decimal = 4.5;
+var string = "Olá Mundo!";
+var lista = ["Abacaxi", "Laranja", "Maçã"];
+
+for (i in lista) {
+    alert(lista[i]);
+}
+*/
+
+// CONSOLE LOG
+console.log("Olá Mundo!")
